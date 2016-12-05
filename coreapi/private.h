7b70508d9591c794ec42a80438a11f067f665994
François Grisez <francois.grisez@belledonne-communications.com>
2016-12-05 16:19:34 +0100

    Turn LinphoneVcart into belle_sip_object_t

--- coreapi/private.h
+++ coreapi/private.h
@@ -1527,6 +1527,7 @@ BELLE_SIP_TYPE_ID(LinphoneNatPolicy),
 BELLE_SIP_TYPE_ID(LinphoneCore),
 BELLE_SIP_TYPE_ID(LinphoneCoreCbs),
 BELLE_SIP_TYPE_ID(LinphoneFactory),
+BELLE_SIP_TYPE_ID(LinphoneVcard)
 BELLE_SIP_DECLARE_TYPES_END
 
 
